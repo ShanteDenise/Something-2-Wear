6e3fd95ce983002777d6a82d8baa6ee3e7fc823a
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@PSADMINs-MacBook-Air.local>
2018-10-26 12:46:14 -0400

    Added seeds file

--- db/seeds.js
+++ db/seeds.js
@@ -1,15 +1,55 @@
 const mongoose = require('./connections')
-const Outfits = require('../models/Outfit')
-const Wardrobe = require('../models/Wardrobe')
+const Tops = require('../models/Tops')
+const Bottoms = require('../models/Bottoms')
+const Shoes = require('../models/Shoes')
+const Accessories = require('../models/Accesories')
+const Closet = require('../models/Closet')
 const User = require('../models/User')
 
-const User1 = new user ({
-    name: "Chizzy",
-    password: "Dreamer94",
-    Wardrobe: [
-        {
 
-        }
-    ]
+const modernShirt = new Tops ({
+        type: "blouse",
+        color: "white",
+        img: 'https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2017/07/18/goods-first-img/1510286471176618651.jpg'
+})
+
+const winterSweater = new Tops ({
+    type: "sweater",
+    color: "red/stripe",
+    img: "https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2017/08/07/goods-first-img/1510287544963574635.jpg"
+})
+const fashionTshirt = new Tops ({
+    type: "graphic tee",
+    color: "grey",
+    img: "https://fashionfabboutique.com/wp-content/uploads/2016/02/Fashionista.png"
+})
+
+const closet1 = new Closet ({
+        tops: [modernShirt, winterSweater, fashionTshirt]
 
 })
+
+const User = new User ({
+    name: 'Shante',
+    password:'favor42',
+    email:'shante.austin@yahoo.com',
+    closet: [closet1]
+})
+
+        
+  
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+    
--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema
 
 const User = new Schema ({
     name: String,
+    email:String,
     password: {
         type:String,
         maxlength:10,
