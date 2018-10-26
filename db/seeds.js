4789bf5dfb00291c1081054aba28a5f1f98b0b49
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@PSADMINs-MacBook-Air.local>
2018-10-26 03:10:43 -0400

    Added seeds

---
+++ db/seeds.js
@@ -0,0 +1,12 @@
+const mongoose = require('./connections')
+const Outfits = require('../models/Outfit')
+const Wardrobe = require('../models/Wardrobe')
+const User = require('../models/User')
+
+const User1 = new user ({
+    name: "Chizzy",
+    password: "Dreamer94",
+    closet: [],
+    outfits: []
+
+})
