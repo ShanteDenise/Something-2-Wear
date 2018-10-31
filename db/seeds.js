6f84a3dafb7d06dd72d65e307472a5bceb2ab439
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@PSADMINs-MacBook-Air.local>
2018-10-31 08:29:07 -0400

    Reseeded data

--- db/seeds.js
+++ db/seeds.js
@@ -7,78 +7,78 @@ const Closet = require('../models/Closet')
 const User = require('../models/User')
 
 const modernShirt = new Tops ({
-        type: "blouse",
-        color: "white",
+        type: "Blouse",
+        color: "White",
         img: 'https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2017/07/18/goods-first-img/1510286471176618651.jpg'
 });
 
 const winterSweater = new Tops ({
-    type: "sweater",
-    color: "red/stripe",
+    type: "Sweater",
+    color: "Red/stripe",
     img: "https://gloimg.zafcdn.com/zaful/pdm-product-pic/Clothing/2017/08/07/goods-first-img/1510287544963574635.jpg"
 });
 
 const fashionTshirt = new Tops ({
-    type: "graphic tee",
-    color: "grey",
+    type: "Graphic tee",
+    color: "Grey",
     img: "https://fashionfabboutique.com/wp-content/uploads/2016/02/Fashionista.png"
 });
 
 const classicSkinny = new Bottoms ({
-    type: "skinny legged jeans",
-    color: "dark blue",
+    type: "Skinny legged jeans",
+    color: "Dark blue",
     img: "https://mbtskoudsalg.com/images/black-skinny-jeans-png-1.png"
 });
 
 const leatherSkirt = new Bottoms ({
-   type: "leather mini skirt",
-   color: "black",
+   type: "Leather mini skirt",
+   color: "Black",
    img:"http://www.toogoodtobebasic.com/files/images/produkter/Fritlagte_produktbilleder_561x650/leather-skirt_miracle_molly_black.png"
 
 });
 
 const miniShorts = new Bottoms ({
-    type: "mini shorts",
-    color: "light blue shorts",
+    type: "Mini shorts",
+    color: "Light blue shorts",
     img:"https://http2.mlstatic.com/shorts-jeans-importado-pronta-entrega-leia-as-medidas-D_NQ_NP_696298-MLB26712892776_012018-F.jpg"
  
  }); 
 
  const camoPants = new Bottoms ({
-    type: "camo pants",
-    color: "green camo pants",
+    type: "Camo Pants",
+    color: "Green Camo Pants",
     img:"https://www.harrysarmysurplus.net/assets/images/rothco/2586_2.jpg"
  
  }); 
 
  const openHeel = new Shoes ({
-     type: "open toe LV shoes",
-     color: "black",
+     type: "Open Toe LV Shoes",
+     color: "Black",
      img: "http://purepng.com/public/uploads/large/purepng.com-christian-louboutin-black-drapesse-peep-toelower-wearshoelouboutindrapesseblack-142152638039518vge.png"
  })
 
  const ankleBoots = new Shoes ({
     type: "Block heel ankle boot",
-    color: "black stripe",
+    color: "Black stripe",
     img: "https://cdn-img.prettylittlething.com/3/1/9/a/319a4dff31f4201f18935c3261b1093841dc7ce3_cly5888_1.jpg?imwidth=1024"
 })
 
 const closedHeel = new Shoes ({
-    type: "closed toe shoe",
-    color: "black",
-    img: "http://purepng.com/public/uploads/large/purepng.com-christian-louboutin-black-drapesse-peep-toelower-wearshoelouboutindrapesseblack-142152638039518vge.png"
+    type: "Huaraches",
+    color: "Galaxy",
+    img: "https://assets.bigcartel.com/product_images/176825561/product_image.jpg?auto=format&fit=max&w=1500"
 })
 
 const redBoots = new Shoes ({
-    type: "red booties",
-    color: "red",
+    type: "Red booties",
+    color: "Red",
     img: "https://melbournechapter.net/images/transparent-booties-clear-plastic-5.png"
 })
 
 const coinPurse = new Accessories ({
-    type: "coin purse",
-    color: "black",
-    img: "https://banner2.kisspng.com/20180206/jae/kisspng-chanel-moschino-handbag-fashion-chanel-black-chanel-bag-lingge-5a7a368ed85cd6.8167302115179587988862.jpg"
+    type: "Purse",
+    color: "Pink",
+    img: "https://sc01.alicdn.com/kf/HTB1LRcYPVXXXXbkXVXXq6xXFXXXn/230003894/HTB1LRcYPVXXXXbkXVXXq6xXFXXXn.jpg"
 })
 const necklace = new Accessories ({
     type: "layed necklace",
